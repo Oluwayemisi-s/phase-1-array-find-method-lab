85eef4bbf30ebf83c1d7156f7e137091f3484b79
Oluwayemisi Soremekun <oluwayemisi.soremekun@gmail.com>
2021-09-22 15:55:24 -0700

    commit tree 5f86ac170968760290f83d209e1a66243c1ba014

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    //{year: "2016", result: "N/A"},
+    {year: "2015", result: "N/A"},
+    {year: "2014", result: "N/A"}
+  ]
 
+function superbowlWin(myArray){
+    let winYear = myArray.find(elements => elements.result === "W")
+    if (winYear)
+        return winYear.year
+    else
+        return undefined
+
+}
+
+// function search(elements){
+//     return elements.result === "W"
+// }
+
+console.log(superbowlWin(record))
